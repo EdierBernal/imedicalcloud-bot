28a6003ececd242028167867fe18468a5ee56d16
Edier José Bernal Rozo <edier.bernal@bitsamericas.com>
2018-07-29 11:43:42 -0500

    Add project files.

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=webappbotimedical-Web-Deploy.pubxml -p:Password=H3CdAe1TalYy1n0Ttaq7Mp3f7fuQANsRXkKAleCrsfEYuSLEnRsqTEA1QfWW
+
